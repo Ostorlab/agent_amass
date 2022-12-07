423aa0a23c330ab83ffd5c2e1d3d95e0620a8f9c
mohsinenar <mnarmouche@gmail.com>
2022-12-07 10:46:50 +0100

    [skip ci] Add black formatter to GitHub workflow.

--- agent/amass_agent.py
+++ agent/amass_agent.py
@@ -11,24 +11,26 @@ from rich import logging as rich_logging
 from agent import amass
 
 logging.basicConfig(
-    format='%(message)s',
-    datefmt='[%X]',
-    level='INFO',
+    format="%(message)s",
+    datefmt="[%X]",
+    level="INFO",
     force=True,
-    handlers=[rich_logging.RichHandler(rich_tracebacks=True)]
+    handlers=[rich_logging.RichHandler(rich_tracebacks=True)],
 )
 logger = logging.getLogger(__name__)
-STORAGE_NAME_WHOIS = b'agent_amass_storage_whois'
-STORAGE_NAME_SUBDOMAIN = b'agent_amass_storage_subdomain'
+STORAGE_NAME_WHOIS = b"agent_amass_storage_whois"
+STORAGE_NAME_SUBDOMAIN = b"agent_amass_storage_subdomain"
 DEFAULT_TIMEOUT_MINUTES = 10
 
 
 class AmassAgent(agent.Agent, agent_persist_mixin.AgentPersistMixin):
     """Amass agent with reverse whois lookup and subdomain enumeration."""
 
-    def __init__(self,
-                 agent_definition: agent_definitions.AgentDefinition,
-                 agent_settings: runtime_definitions.AgentSettings) -> None:
+    def __init__(
+        self,
+        agent_definition: agent_definitions.AgentDefinition,
+        agent_settings: runtime_definitions.AgentSettings,
+    ) -> None:
 
         agent.Agent.__init__(self, agent_definition, agent_settings)
         agent_persist_mixin.AgentPersistMixin.__init__(self, agent_settings)
@@ -40,35 +42,46 @@ class AmassAgent(agent.Agent, agent_persist_mixin.AgentPersistMixin):
         Args:
             message: The received message.
         """
-        logger.info('processing message of selector : %s', message.selector)
-        domain_name = message.data['name']
-        canonalized_domain = tld.get_tld(domain_name, as_object=True, fix_protocol=True, fail_silently=True)
+        logger.info("processing message of selector : %s", message.selector)
+        domain_name = message.data["name"]
+        canonalized_domain = tld.get_tld(
+            domain_name, as_object=True, fix_protocol=True, fail_silently=True
+        )
         if canonalized_domain is None:
             return
 
         canonalized_domain = canonalized_domain.fld
 
         if self.set_add(STORAGE_NAME_WHOIS, canonalized_domain) is True:
-            logger.info('Collecting domains using reverse whois lookup for %s:', domain_name)
+            logger.info(
+                "Collecting domains using reverse whois lookup for %s:", domain_name
+            )
             subdomains = amass.intel_whois(domain_name, timeout=DEFAULT_TIMEOUT_MINUTES)
             for sub in subdomains:
-                logger.info('Found: %s', sub)
+                logger.info("Found: %s", sub)
                 # Reverse whois lookup will yield the same result for all. This is to avoid processing the same domains.
                 self.set_add(STORAGE_NAME_WHOIS, sub)
-                self.emit(selector='v3.asset.domain_name', data={'name': sub})
+                self.emit(selector="v3.asset.domain_name", data={"name": sub})
         else:
-            logger.info('WHOIS %s has already been processed. skipping for now.', domain_name)
+            logger.info(
+                "WHOIS %s has already been processed. skipping for now.", domain_name
+            )
 
         if self.set_add(STORAGE_NAME_SUBDOMAIN, canonalized_domain) is True:
-            logger.info('Collecting subdomains using enumeration for %s:', domain_name)
-            subdomains = amass.enum_subdomain(domain_name, timeout=DEFAULT_TIMEOUT_MINUTES)
+            logger.info("Collecting subdomains using enumeration for %s:", domain_name)
+            subdomains = amass.enum_subdomain(
+                domain_name, timeout=DEFAULT_TIMEOUT_MINUTES
+            )
             for sub in subdomains:
-                logger.info('Found: %s', sub)
-                self.emit(selector='v3.asset.domain_name', data={'name': sub})
+                logger.info("Found: %s", sub)
+                self.emit(selector="v3.asset.domain_name", data={"name": sub})
         else:
-            logger.info('SUBDOMAIN %s has already been processed. skipping for now.', domain_name)
+            logger.info(
+                "SUBDOMAIN %s has already been processed. skipping for now.",
+                domain_name,
+            )
 
 
-if __name__ == '__main__':
-    logger.info('starting agent ...')
+if __name__ == "__main__":
+    logger.info("starting agent ...")
     AmassAgent.main()
--- tests/conftest.py
+++ tests/conftest.py
@@ -11,24 +11,24 @@ from agent import amass_agent
 
 @pytest.fixture
 def scan_message():
-    """Creates a dummy message of type v3.asset.domain_name to be used by the agent for testing purposes.
-    """
-    selector = 'v3.asset.domain_name'
+    """Creates a dummy message of type v3.asset.domain_name to be used by the agent for testing purposes."""
+    selector = "v3.asset.domain_name"
     msg_data = {
-        'name': 'ostorlab.co',
+        "name": "ostorlab.co",
     }
     return message.Message.from_data(selector, data=msg_data)
 
 
 @pytest.fixture
 def test_agent():
-    with (pathlib.Path(__file__).parent.parent / 'ostorlab.yaml').open() as yaml_o:
+    with (pathlib.Path(__file__).parent.parent / "ostorlab.yaml").open() as yaml_o:
         definition = agent_definitions.AgentDefinition.from_yaml(yaml_o)
         settings = runtime_definitions.AgentSettings(
-            key='agent/ostorlab/amass',
-            bus_url='NA',
-            bus_exchange_topic='NA',
-            redis_url='redis://redis',
+            key="agent/ostorlab/amass",
+            bus_url="NA",
+            bus_exchange_topic="NA",
+            redis_url="redis://redis",
             args=[],
-            healthcheck_port=5301)
+            healthcheck_port=5301,
+        )
         return amass_agent.AmassAgent(definition, settings)
