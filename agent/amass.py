9f8f158c099e21c83f0428e67519ef2b7c2e7002
Alaeddine Mesbahi <alaeddine.mesbahi@ostorlab.dev>
2023-10-26 19:46:15 +0100

    Fix amass command.

--- agent/amass.py
+++ agent/amass.py
@@ -1,9 +1,9 @@
 """Wrapper for amass binary."""
-from typing import List, Optional
+from typing import Iterator
 import subprocess
 
 
-def intel_whois(domain: str, timeout: Optional[int] = None) -> List[str]:
+def intel_whois(domain: str, timeout: int | None = None) -> list[str]:
     """Perform whois reverse lookup.
 
     Args:
@@ -30,8 +30,8 @@ def intel_whois(domain: str, timeout: Optional[int] = None) -> List[str]:
         return []
 
 
-def enum_subdomain(domain: str, timeout: Optional[int] = None) -> List[str]:
-    """Active subdomain enumeration with IP resolution to rule out invalid or old subdomains..
+def enum_subdomain(domain: str, timeout: int | None = None) -> Iterator[str]:
+    """Active subdomain enumeration with IP resolution to rule out invalid or old subdomains.
 
     Args:
         domain: Domain to lookup.
@@ -40,18 +40,21 @@ def enum_subdomain(domain: str, timeout: Optional[int] = None) -> List[str]:
     Returns:
         List of subdomains.
     """
-    command = ["amass", "enum", "-brute", "-min-for-recursive", "2", "-d", domain]
+    command = ["amass", "enum", "-brute", "-norecursive", "-d", domain]
     if timeout is not None:
         command.extend(["-timeout", str(timeout)])
 
-    try:
-        # The timeout is not always respected by the amass, we enforce ours on top of it with an extra minute.
-        result = subprocess.run(
-            command, capture_output=True, check=False, timeout=(timeout + 1) * 60
-        )
-        if result.returncode != 0:
-            return []
-        else:
-            return result.stdout.decode().splitlines()
-    except subprocess.TimeoutExpired:
-        return []
+    # The timeout is not always respected by the amass, we enforce ours on top of it with an extra minute.
+    process = subprocess.Popen(
+        command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, shell=True
+    )
+    while True:
+        output = process.stdout.readline()
+        if output == '' and process.poll() is not None:
+            break
+        print("XXX", output)
+        if output:
+            if "FQDN" in output:
+                yield output.strip().split(" ")[0]
+
+
